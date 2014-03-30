013d04611dd543150cda2a586c73a96b44622266
Matthew Dickinson <mdickinson@sevone.com>
2014-03-30 20:34:01 +0000

    Simple lists

---
+++ example.py
@@ -0,0 +1,39 @@
+
+# Basic lists
+print 'Basic lists \n'
+print( 'myvar = [ 1, 2, 3 ]' )
+myvar = [ 1, 2, 3, ]
+print myvar
+
+print ''
+
+#print myvar
+
+# You can add things to myvar!
+print 'myvar.append( 4 )'
+myvar.append( 4 )
+print myvar
+
+print ''
+
+print 'You can also make lists of lists...'
+myvar = [ [ 'one', 'two', 'three' ], [ 'four', 'five', 'six' ], [ 'seven', 'eight', 'nine' ] ]
+print "myvar = [ [ 'one', 'two', 'three' ], [ 'four', 'five', 'six' ], [ 'seven', 'eight', 'nine' ] ]"
+print myvar
+
+print ''
+
+print 'And now you can reference any part of those lists by index. All lists are index by 0.'
+print 'myvar[ 0 ]'
+print myvar[ 0 ]
+
+print ''
+
+print 'myvar[ 0 ][ 1 ]'
+print myvar[ 0 ][ 1 ]
+
+print ''
+
+print 'For more on lists: http://docs.python.org/2/tutorial/datastructures.html'
+
+
