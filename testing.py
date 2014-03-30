852f6394bbeaaa9ed7d59cfd87ffdd523be5630f
Matthew Dickinson <mdickinson@sevone.com>
2014-03-30 21:38:37 +0000

    Adding test script

---
+++ testing.py
@@ -0,0 +1,16 @@
+def calculate( val1, val2, val3, val4, val5, val6 ):
+	
+	returnVal = {
+		'ret1': val1,
+		'ret2': val2,
+		'ret3': val3,
+		'ret4': val4,
+		'ret5': val5,
+		'ret6': val6
+	}
+
+	return returnVal
+
+val = calculate( 'a', 'b', 'c', 'd', 'e', 'f' )
+print val
+
